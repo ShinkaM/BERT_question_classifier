2d60dc7867a4604e95d4375f4a7d620931b0d7e3
Mori, Shinka | CXD <ts-shinka.mori@rakuten.com>
2025-02-26 20:16:38 +0900

    modified to add val set

--- scoville_nlp_assignment/lora_finetune.py
+++ scoville_nlp_assignment/lora_finetune.py
@@ -1,71 +1,57 @@
-# %%
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from datasets import load_dataset
 import torch
 import evaluate
-
-# %%
 import torch
-if torch.backends.mps.is_available():
-    mps_device = torch.device("mps")
-    x = torch.ones(1, device=mps_device)
-    print(x)
-else:
-    print ("MPS device not found.")
-
-# %%
-import pandas as pd
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
-from datasets import Dataset
 from peft import LoraConfig, get_peft_model, TaskType
+import numpy as np
+from datasets import Dataset
 
-# Load your dataset from CSV files
-def load_data(train_csv, test_csv):
-    train_df = pd.read_csv(train_csv)
-    test_df = pd.read_csv(test_csv)
+"""
+Load the dataset from pandas and convert boolean to int for easier computation
+"""
+def load_data(train_csv):
+    df = pd.read_csv(train_csv)
+    
+    df["is_question"] = df["is_question"].astype(int)
 
-    # Convert labels to integers (0 or 1)
-    train_df["is_question"] = train_df["is_question"].astype(int)
+    msk = np.random.rand(len(df)) < 0.8
+    train = df[msk]
+    val = df[~msk]
+    train_dataset = Dataset.from_pandas(train)
+    val_dataset = Dataset.from_pandas(val)
+    return train_dataset, val_dataset
 
-    # Convert the DataFrames to Hugging Face Datasets
-    train_dataset = Dataset.from_pandas(train_df)
-    test_dataset = Dataset.from_pandas(test_df)
+"""
+Preprocess data by tokenizing the sentences and relabeling the column.
+The tokenizer base for the model used "cl-tohoku/bert-base-japanese" is MeCab
+"""
 
-    return train_dataset, test_dataset
-
-
-
-
-# %%
-def preprocess_data(dataset, tokenizer, train = True):
+def preprocess_data(dataset, tokenizer):
     def tokenize_function(examples):
         return tokenizer(examples["sentence"], padding="max_length", truncation=True, max_length=128)
-    
     dataset = dataset.map(tokenize_function, batched=True)
-    if train:
-        dataset = dataset.rename_column("is_question", "labels")  # Rename label column to "labels" for Hugging Face compatibility
-        dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
-    else:
-        dataset.set_format(type="torch", columns=["input_ids", "attention_mask"])
-
+    dataset = dataset.rename_column("is_question", "labels")  # Rename label column to "labels" for Hugging Face compatibility
+    dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
     return dataset
 
-# %%
+"""
+Training code
+Execute the code below to train the model using LORA; 100iters/60sec on Mac M2
+"""
+
 model_name = "cl-tohoku/bert-base-japanese"
 
-# model_name =  "google-bert/bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 model.to(device)
-# Load and preprocess the dataset
-train_dataset, test_dataset = load_data('datasets/train.csv', 'datasets/test.csv')
-train_dataset = preprocess_data(train_dataset, tokenizer, True)
-test_dataset = preprocess_data(test_dataset, tokenizer, False)
 
-# %%
+train_dataset, val_dataset = load_data('datasets/train.csv')
+train_dataset = preprocess_data(train_dataset, tokenizer)
+val_dataset = preprocess_data(val_dataset, tokenizer)
+
 lora_config = LoraConfig(
         task_type=TaskType.SEQ_CLS,  # Sequence classification task
         inference_mode=False,
@@ -74,10 +60,8 @@ lora_config = LoraConfig(
         lora_dropout=0.1,  # Dropout rate
     )
 
-# %%
 model = get_peft_model(model, lora_config)
 
-# %%
 training_args = TrainingArguments(
         output_dir="./results",          # Output directory
         evaluation_strategy="epoch",    # Evaluate every epoch
@@ -93,7 +77,6 @@ training_args = TrainingArguments(
         metric_for_best_model = 'eval_samples_per_second'
     )
 
-# %%
 def compute_metrics(eval_pred):
     from sklearn.metrics import accuracy_score, precision_recall_fscore_support
     logits, labels = eval_pred
@@ -106,16 +89,52 @@ trainer = Trainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=test_dataset,
+        eval_dataset=val_dataset,
         tokenizer=tokenizer,
-        compute_metrics=compute_metrics,  # Optional: Define a function to compute metrics
+        compute_metrics=compute_metrics, 
     )
 
-    # Train the model
 trainer.train()
 
-# %%
+model.save_pretrained("./lora_bert_model_v2")
 
 
+"""
+Load fine-tuned model
 
-model.save_pretrained("./lora_bert_model")
+
+MODEL_NAME = "./results/checkpoint-4886"
+model_name = "cl-tohoku/bert-base-japanese"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
+def predict_intent(text, model, tokenizer):
+    inputs = tokenizer(
+        text,
+        return_tensors="pt",
+        truncation=True,
+        padding="max_length",
+        max_length=128,
+    ).to(device)
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=-1).item()
+    return predicted_class
+
+# device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu") ##if mps is available
+device = torch.device("cuda:0" if torch.cuda.is_available()  else "cpu" )
+ 
+df = pd.read_csv('datasets/test.csv')
+
+results = []        
+for t in df['sentence']:
+    results.append(predict_intent(t, model, tokenizer))
+
+results = [True if i else False for i in results]
+
+df['is_question'] = results
+
+df.to_csv('output.csv', index=None)
+"""
