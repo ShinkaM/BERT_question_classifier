072ac1efc834e87f572bbe0a963bc9a39eea9707
Shinka <shinka@Shinkas-MacBook-Air.local>
2025-02-27 23:17:58 +0900

    update model

--- scoville_nlp_assignment/lora_finetune.py
+++ scoville_nlp_assignment/lora_finetune.py
@@ -7,6 +7,8 @@ import torch
 from peft import LoraConfig, get_peft_model, TaskType
 import numpy as np
 from datasets import Dataset
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
 
 """
 Load the dataset from pandas and convert boolean to int for easier computation
@@ -15,7 +17,7 @@ def load_data(train_csv):
     df = pd.read_csv(train_csv)
     
     df["is_question"] = df["is_question"].astype(int)
-
+    #make train and val set, using 80% for training
     msk = np.random.rand(len(df)) < 0.8
     train = df[msk]
     val = df[~msk]
@@ -34,7 +36,7 @@ def preprocess_data(dataset, tokenizer):
     def tokenize_function(examples):
         return tokenizer(examples["sentence"], padding="max_length", truncation=True, max_length=128)
     dataset = dataset.map(tokenize_function, batched=True)
-    dataset = dataset.rename_column("is_question", "labels")  # Rename label column to "labels" for Hugging Face compatibility
+    dataset = dataset.rename_column("is_question", "labels")  # Rename for Hugging Face compatibility
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
     return dataset
 
@@ -48,7 +50,9 @@ model_name = "cl-tohoku/bert-base-japanese"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
-device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu") 
+# device = torch.device("cuda:0" if torch.cuda.is_available()  else "cpu" )
+
 model.to(device)
 
 train_dataset, val_dataset = load_data('datasets/train.csv')
@@ -66,22 +70,21 @@ lora_config = LoraConfig(
 model = get_peft_model(model, lora_config)
 
 training_args = TrainingArguments(
-        output_dir="./results",          # Output directory
-        evaluation_strategy="epoch",    # Evaluate every epoch
-        learning_rate=2e-5,             # Learning rate
-        per_device_train_batch_size=20, # Batch size for training
-        per_device_eval_batch_size=20,  # Batch size for evaluation
-        num_train_epochs=3,             # Number of epochs
-        weight_decay=0.01,              # Weight decay
-        logging_dir="./logs",           # Directory for logging
-        logging_steps=10,               # Log every 10 steps
-        save_strategy="epoch",          # Save model every epoch
-        load_best_model_at_end=True,    # Load the best model at the end of training
+        output_dir="./results",         
+        evaluation_strategy="epoch",   
+        learning_rate=2e-5,             
+        per_device_train_batch_size=20, 
+        per_device_eval_batch_size=20,  
+        num_train_epochs=3,             
+        weight_decay=0.01,              
+        logging_dir="./logs",          
+        logging_steps=10,              
+        save_strategy="epoch",        
+        load_best_model_at_end=True,   
         metric_for_best_model = 'eval_samples_per_second'
     )
 
 def compute_metrics(eval_pred):
-    from sklearn.metrics import accuracy_score, precision_recall_fscore_support
     logits, labels = eval_pred
     predictions = torch.argmax(torch.tensor(logits), dim=-1)
     precision, recall, f1, _ = precision_recall_fscore_support(labels, predictions, average="binary")
@@ -107,11 +110,11 @@ model.save_pretrained("./lora_bert_model_v2")
 Load fine-tuned model
 
 """
-MODEL_NAME = "./lora_bert_model_v2"
+MODEL_NAME = "./lora_bert_model_v2"#path to finetuned model
 model_name = "cl-tohoku/bert-base-japanese"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
 
 def predict_intent(text, model, tokenizer):
     inputs = tokenizer(
